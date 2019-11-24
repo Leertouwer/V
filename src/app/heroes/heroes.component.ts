2701e21e0ed6acea3be22c6182f9d0797008b194
Vleer <Vleer@users.noreply.github.com>
2019-11-24 13:43:32 +0100

    My heroes list complete

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,11 +8,16 @@ import {HEROES} from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  // Hero: hero{
+  //   id = 1;
+  //   name = "Windstorm";
+  // }
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
   heroes = HEROES;
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  
   constructor() { }
   
   ngOnInit() {
